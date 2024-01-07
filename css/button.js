d222762856a7b6b2049a8c4604180e318275b476
ann3v <151371969+ann3v@users.noreply.github.com>
2024-01-07 23:44:09 +0100

    Add files via upload

---
+++ css/button.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var backToTopBtn = document.getElementById('aa-backToTopBtn');
+
+  window.addEventListener('scroll', function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      backToTopBtn.style.display = 'block';
+    } else {
+      backToTopBtn.style.display = 'none';
+    }
+  });
+
+  backToTopBtn.addEventListener('click', function () {
+    scrollToTop(600); 
+  });
+
+  function scrollToTop(duration) {
+    var start = window.pageYOffset;
+    var startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+
+    function animateScroll() {
+      var now = 'now' in window.performance ? performance.now() : new Date().getTime();
+      var elapsedTime = now - startTime;
+
+      window.scroll(0, easeInOutCubic(elapsedTime, start, -start, duration));
+
+      if (elapsedTime < duration) {
+        requestAnimationFrame(animateScroll);
+      }
+    }
+
+    function easeInOutCubic(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t * t + b;
+      t -= 2;
+      return c / 2 * (t * t * t + 2) + b;
+    }
+
+    requestAnimationFrame(animateScroll);
+  }
+});
